196eb3e22ce6c830a07ed0394564a8dd43798306
Filip Marić <filip@matf.bg.ac.rs>
2021-08-23 00:24:45 +0200

    Sitno doterivanje Nebojšine revizije

--- _src/06_hello_request/app.py
+++ _src/06_hello_request/app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route("/pretraga")
 def search():
     vrsta = request.args.get("vrsta", "svi proizvodi")
-    max_cena = request.args.get("max_cena", float('inf'))
+    max_cena = request.args.get("max_cena", float('inf'), type=float)
     return render_template("search.html",
                            vrsta=vrsta, max_cena=max_cena)
